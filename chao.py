ff0c41914929826d2031dab3fb5bd28bb4ee764e
Handryo <99217246+Handryo@users.noreply.github.com>
2022-04-10 16:00:21 -0300

    Update chao.py

--- chao.py
+++ chao.py
@@ -1,20 +1,20 @@
 import pygame.sprite
-from config import Largura, Altura
+from config import height, width
 
 
-class Chao(pygame.sprite.Sprite):
-    def __init__(self, piso, pos_x):
+class Floor(pygame.sprite.Sprite):
+    def __init__(self, floor, pos_x):
         super().__init__()
-        self.image = piso
+        self.ticks = None
+        self.image = floor
         self.image = pygame.transform.scale(self.image, (32*3, 32*3))
         self.rect = self.image.get_rect()
         self.rect.x = pos_x * 96
-        self.rect.y = Altura - 96
+        self.rect.y = height - 96
 
     def update(self):
-        ticks = pygame.time.get_ticks()
+        self.ticks = pygame.time.get_ticks()
         self.rect.x = self.rect.x - 5
         if self.rect.x < -96:
-            self.rect.x = Largura + 96
-        print(ticks)
-
+            self.rect.x = width + 96
+            
