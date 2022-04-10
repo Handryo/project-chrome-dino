eb84320a9fa83273a1c8169c8d7ae383de7b3e24
Handryo <99217246+Handryo@users.noreply.github.com>
2022-04-10 15:59:30 -0300

    Update game.py

--- game.py
+++ game.py
@@ -1,22 +1,26 @@
-
 from config import *
-from chao import *
+from floor import *
+from dino import *
 pygame.init()
-tela = pygame.display.set_mode((Largura, Altura))
-pygame.display.set_caption("Frog Adventure")
+screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption("Dino run")
 
-todas_as_sprites = pygame.sprite.Group()
-for i in range(Largura * 3 // 96):
-    chao_ = Chao(chao_1, i)
-    todas_as_sprites.add(chao_)
+#for i in range(width * 3 // 96):
+    #chao_ = Floor(chao_1, i)
+    #todas_as_sprites.add(chao_)
+
+Dino = Dino()
 
 while True:
-    relogio.tick(30)
-    tela.fill(BRANCO)
+    clock.tick(30)
+    screen.fill(white)
     for e in pygame.event.get():
         if e.type == pygame.QUIT:
             exit()
-    todas_as_sprites.draw(tela)
-    todas_as_sprites.update()
+    all_sprites.draw(screen)
+    all_sprites.update()
+    #todas_as_sprites.draw(tela)
+    #todas_as_sprites.update()
     pygame.display.flip()
+
 
