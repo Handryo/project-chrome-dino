677882d3b066ef0daa8c95e06b43ed0952f13a11
Handryo <99217246+Handryo@users.noreply.github.com>
2022-04-10 23:14:10 -0300

    Create cloud.py

---
+++ cloud.py
@@ -0,0 +1,18 @@
+import pygame.sprite
+from config import sprite_cloud, width
+from random import randrange
+
+class Cloud(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        img = sprite_cloud
+        self.image = pygame.transform.scale(img, (32 * 2, 32 * 2))
+        self.rect = self.image.get_rect()
+        self.rect.x = width - randrange(30, 300, 90)
+        self.rect.y = randrange(50, 200, 50)
+
+    def update(self):
+        if self.rect.topright[0] < 0:
+            self.rect.x = width
+            self.rect.y = randrange(50, 200, 50)
+        self.rect.x -= 10
