8daaaecc47ace0fd4717d25c25f49f49e2deeac8
Handryo <99217246+Handryo@users.noreply.github.com>
2022-04-11 00:27:29 -0300

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import pygame
 
-width = 800
-height = 700
+width = 640
+height = 600
 
 # Colors
 white = (255, 255, 255)
@@ -12,4 +12,8 @@ clock = pygame.time.Clock()
 # Sprites
 sprite_dino = pygame.image.load("img/Dino.png")  # Dino
 sprite_cloud = pygame.image.load("img/Cloud_sprite.png")  # Cloud
+sprite_floor = pygame.image.load("img/Floor_sprite.png")  # Floor
+sprite_flydino = pygame.image.load("img/Obstacle_Dino.png")  # Fly Dino
+sprite_cactus = pygame.image.load("img/Obstacle_Cactus.png")  # Cactus
 
+all_sprites = pygame.sprite.Group() # Add images to sprite group
