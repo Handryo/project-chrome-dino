07e1edfbae1b204bda48e55e6fcbcd234cb1f46c
Handryo <99217246+Handryo@users.noreply.github.com>
2022-04-11 04:53:19 -0300

    Update game.py

--- game.py
+++ game.py
@@ -1,14 +1,27 @@
 from pygame import *
+from cactus import *
 from cloud import *
 from config import *
 from dino import *
 from floor import *
+from mensages import *
+
+
+def restart_game():
+    cactus.rect.x = width
+    
 
 pygame.init()
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Dino run")
 
 Dino = Dino()
+Mensages = Mensages()
+
+for cact in range(2):
+    cactus = Cactus()
+    all_sprites.add(cactus)
+    obstacles_group.add(cactus)
 
 for c in range(4):
     cloud = Cloud()
@@ -18,6 +31,7 @@ for f in range(640 * 2 // 64):
     floor = Floor(f)
     all_sprites.add(floor)
 
+colision = False
 while True:
     clock.tick(30)
     screen.fill(white)
@@ -30,6 +44,8 @@ while True:
                     pass
                 else:
                     dino.leap()
+                if event.key == K_r and colision == True:
+                    restart_game()
 
     if key.get_pressed()[K_DOWN]:
         dino.duck()
@@ -37,6 +53,17 @@ while True:
         if dino.down:
             dino.undunking()
 
+    colisions = pygame.sprite.spritecollide(dino, obstacles_group, False, pygame.sprite.collide_mask)
     all_sprites.draw(screen)
-    all_sprites.update()
+    if colisions and colision == False:
+        colision = True
+
+    if colision:
+        dino.dead()
+        game_over = Mensages.show_text('GAME OVER', 35, black)
+        screen.blit(game_over, (width // 2 - 150, height // 2 - 150))
+        restart = Mensages.show_text('Press "r" to restart', 20, black)
+        screen.blit(restart, (width // 2 - 200, height // 2 - 90))
+    else:
+        all_sprites.update()
     pygame.display.flip()
